d570373ce1c865789a0607ada53d3c4d58a67101
Mohammed Hamad <mahamad2@ualberta.ca>
2026-09-16 18:17:11 -0600

    Complete Lab 3

---
+++ code/app/src/main/java/com/example/listycity3/CityListScreen.kt
@@ -0,0 +1,163 @@
+package com.example.listycity3
+import androidx.compose.ui.Alignment
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.listycity3.ui.theme.ListyCity3Theme
+
+@Composable
+fun CityListScreen(
+    cities: List<City>,
+    onAddCity: (City) -> Unit,
+    onDeleteCity: (City) -> Unit,
+    onUpdateCity: (City, City) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var newCityName by remember { mutableStateOf("") }
+    var newProvince by remember { mutableStateOf("") }
+    var showAddCity by remember { mutableStateOf(false)}
+    var selectedCity by remember { mutableStateOf<City?>(null)}
+
+    Column(modifier = modifier) {
+        FloatingActionButton(
+            onClick = { showAddCity = true },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text("+")
+        }
+        if  (showAddCity) {
+
+            Row {
+                OutlinedTextField(
+                    value = newCityName,
+                    onValueChange = { newCityName = it },
+                    label = { Text("City Name") },
+                    modifier = Modifier.weight(1f)
+                )
+                OutlinedTextField(
+                    value = newProvince,
+                    onValueChange = { newProvince = it },
+                    label = { Text("Province") },
+                    modifier = Modifier.weight(1f)
+
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+                Button(
+                    onClick = {
+                        if (selectedCity != null) {
+                            onUpdateCity(
+                                selectedCity!!,
+                                City(newCityName, newProvince)
+                            )
+                            selectedCity = null
+
+                        } else {
+
+                            onAddCity(City(newCityName, newProvince))
+                        }
+                        newCityName = ""
+                        newProvince = ""
+                        showAddCity = false
+
+                    }
+
+                ) {
+                    Text(
+                        if (selectedCity != null) "Update" else "Add"
+                    )
+                }
+            }
+        }
+        LazyColumn{
+            itemsIndexed(cities) { index, city ->
+                CityRow(
+                    city = city,
+                    onDelete = { onDeleteCity(city) },
+                    onClick = {
+                        selectedCity = city
+                        newCityName = city.name
+                        newProvince = city.province
+                        showAddCity = true
+                    }
+                )
+
+                if (index < cities.lastIndex) {
+                    HorizontalDivider()
+                }
+            }
+        }
+
+    }
+}
+
+@Composable
+fun CityRow(
+    city: City,
+    onDelete: () -> Unit,
+    onClick: () -> Unit
+
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() }
+            .padding(horizontal = 20.dp, vertical = 16.dp)
+    ) {
+        Text(
+            text = city.name,
+            fontSize = 20.sp,
+            modifier = Modifier.weight(1f)
+        )
+
+        Text(
+            text = city.province,
+            fontSize = 20.sp,
+            modifier = Modifier.weight(1f)
+        )
+        Button(onClick = onDelete) {
+            Text(" Delete ")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CityListScreenPreview() {
+    ListyCity3Theme {
+        CityListScreen(
+            cities = listOf(
+                City("Edmonton", "AB"),
+                City("Vancouver", "BC"),
+                City("Calgary", "AB")
+            ),
+            onAddCity = {},
+            onDeleteCity = {},
+            onUpdateCity = { _, _ -> }
+
+        )
+    }
+}
---
+++ code/app/src/main/java/com/example/listycity3/CityRepository.kt
@@ -0,0 +1,28 @@
+package com.example.listycity3
+
+import androidx.compose.runtime.mutableStateListOf
+
+class CityRepository {
+    private val _cities = mutableStateListOf(
+        City("Edmonton", "AB"),
+        City("Vancouver", "BC"),
+        City("Toronto", "ON")
+    )
+
+
+    val cities: List<City>
+        get() = _cities
+    fun addCity(city: City) {
+        _cities.add(city)
+    }
+    fun deleteCity(city: City) {
+        _cities.remove(city)
+    }
+    fun updateCity(oldCity: City, updatedCity: City) {
+        val index = _cities.indexOf(oldCity)
+
+        if (index != -1) {
+            _cities[index] = updatedCity
+        }
+    }
+}
